50c2b8fded233e81c70eb30a20c6778a924e58f7
jwilksftw <jwilksftw@users.noreply.github.com>
2019-08-17 13:06:47 -0400

    simplified

--- imageInImage.py
+++ imageInImage.py
@@ -16,22 +16,18 @@ from pyclick import HumanClicker
 time.sleep(2)
 hc = HumanClicker()
 def bobberfish():
-    #a=0
         b=0
-    #This box collects images and moves to find the bobber
-    #for i in range(1350, 15, 100):
-        #a+=1
         for c in range(300, 850, 70):
             b+=1
             #Grabs the whole screen to search for 'Fishing Bobber' Tooltip
             im=ImageGrab.grab(bbox=(0,0,2560,1440))
-            im.save('./ImageInImage/assets/im'+str(b)+'.png')
+            im.save('im'+str(b)+'.png')
             # Image we want to search
-            img_rgb = cv.imread('./ImageInImage/assets/im'+str(b)+'.png')
+            img_rgb = cv.imread('im'+str(b)+'.png')
             # Make image grey to remove some variation
             img_grey = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
             # Image we want to find
-            template = cv.imread('./ImageInImage/assets/cog2.png',0)
+            template = cv.imread('cog2.png',0)
             # Match grey image to template
             w, h = template.shape[::-1]
             res = cv.matchTemplate(img_grey,template,cv.TM_CCOEFF_NORMED)
@@ -42,7 +38,6 @@ def bobberfish():
             
             # Draw red box around the item we think matches and starts listening for fish sound
             for pt in zip(*loc[::-1]):
-                cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
                 #Fish sound listen
                 FORMAT=pyaudio.paInt16
                 CHANNELS=2
@@ -50,15 +45,14 @@ def bobberfish():
                 CHUNK=1024
                 RECORD_SECONDS=1500
                 FILE_NAME="RECORDING.wav"
-
                 audio=pyaudio.PyAudio() #instantiate the pyaudio
-
+                
                 #recording prerequisites
                 stream=audio.open(format=FORMAT,channels=CHANNELS, 
                                   rate=RATE,
                                   input=True,
                                   frames_per_buffer=CHUNK)
-
+                
                 #starting recording
                 frames=[]
                 for i in range(0,100):
@@ -79,10 +73,6 @@ def bobberfish():
                         if i==100:
                             return
 
-
-
-            # Write image with red box to file
-            cv.imwrite('./ImageInImage/assets/foundCog.png',img_rgb)
             #Moves if the bobber hasn't been found
             hc.move((1310, c),.2)
 
